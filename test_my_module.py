547b579bd35b990548c9483de7cd1006eedee861
FranciscoImanolSuarez <imanolsuarez98@gmail.com>
2025-07-10 19:47:15 -0300

    Fix flake8 and black formatting issues in Python files

--- test_my_module.py
+++ test_my_module.py
@@ -1,15 +1,13 @@
 from my_module import add, subtract
 
+
 def test_add():
-    assert add(1,2) == 3
-    assert add(-1,1) == 0
-    assert add(0,0) == 0
-    assert add(40,40) == 80
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
 
 
 def test_subtract():
-    assert subtract(5,2) == 3
-    assert subtract(10,10) == 0
-    assert subtract(0,5) == -5
-
-
+    assert subtract(5, 2) == 3
+    assert subtract(10, 10) == 0
+    assert subtract(0, 5) == -5
